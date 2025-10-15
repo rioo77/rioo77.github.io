af5861e6312108db792c817f4246aed3e0b93d46
rioo77 <2759071241@qq.com>
2025-10-15 09:03:41 +0800

    Site updated: 2025-10-15 09:03:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/15/test/","2025/10/13/我的第一篇博客/","2025/10/13/hello-world/"];function toRandomPost(){
+var posts=["2025/10/15/test/","2025/10/13/我的第一篇博客/","2025/10/13/hello-world/","2025/10/15/aaa/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
